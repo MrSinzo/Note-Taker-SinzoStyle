5cc18f6470986e033634471a8df6f7db1cf5e778
Kyle Feldman <flip93wolf@gmail.com>
2022-12-11 12:33:57 -0800

    Got Db file to update correctly, now i just need it to display

--- server.js
+++ server.js
@@ -1,42 +1,41 @@
-const express = require("express")
+const express = require("express");
 const fs = require("fs");
-const { Server } = require("http");
-const path = require('path');
-const uuid = require("uuid")
+// const { Server } = require("http");
+const path = require("path");
+const uuid = require("./helpers/uuid");
 const app = express();
 const PORT = 3002;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // line 53 of readme/criteria
-app.get('/notes', (req, res) => {
- /* Error: ENOENT: no such file or directory, stat 'C:\Users\flip
-9\bootcamp\Note-Taker-SinzoStyle\notes.html'*/
-  res.sendFile(path.join(__dirname, 'public/notes.html')); 
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-
 // line 55 of readme/criteria
-app.get('*', (req, res) =>{ 
-  res.sendFile(path.join(__dirname, 'public/index.html'))  
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 /*api routes*/
+
 // line 59 of readme/criteria
-app.get('/api/notes', (req, res) => {   
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+app.get("./api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
       console.error(err);
     } else {
       // Convert string into JSON object
-      console.log("line 33 - data was read")
-    }})
+      return res.json(path.join(__dirname, "public/notes.html"))
+    }
+  });
 });
 
-// line 61? of readme/criteria
-app.post('/api/notes', (req, res) =>{ 
 
+// line 61? of readme/criteria // successfully updating db.json now
+app.post("/api/notes", (req, res) => {
   console.info(`${req.method} request received to add a Note/Task`);
 
   // Destructuring assignment for the items in req.body
@@ -48,39 +47,37 @@ app.post('/api/notes', (req, res) =>{
     const newNote = {
       title,
       text,
-      // review_id: uuid(),
+      review_id: uuid(), //pulled in helper function from act. 20 
     };
 
-  fs.readFile('/db/db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      // Convert string into JSON object
-      const parsedDb = JSON.parse(data)
-      parsedDb.push(newNote)
-      // res.json(path.join(parsedDb, 'db.json'))
-      fs.writeFile(
-        '/db/db.json',
-        JSON.stringify(parsedDb, null, 4),
-        (writeErr) =>
-          writeErr
-            ? console.error(writeErr)
-            : console.info('Successfully updated reviews!')
-      )
-    }})
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedDb = JSON.parse(data);
+        parsedDb.push(newNote);
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(parsedDb, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info("Successfully updated Notes!")
+        );
+      }
+    });
     const response = {
-      status: 'success',
+      status: "success",
       body: newNote,
     };
 
     console.log(response);
     res.status(201).json(response);
   } else {
-    res.status(500).json('Error in posting review');
+    res.status(500).json("Error in posting review");
   }
 });
-
-
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
