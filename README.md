7c0d359e06b6c72981de680254cfd3e52db180f0
Kyle Feldman <flip93wolf@gmail.com>
2022-12-13 13:06:28 -0800

    realized heroku isnt working and database is not displaying correctly

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Note-Taker-SinzoStyle
 A simple program that will help keep track of chores and or "to-do's"!
+
+
+## User Story
+AS A small business owner
+I WANT to be able to write and save notes
+SO THAT I can organize my thoughts and keep track of tasks I need to complete
+
+## Table of contents
+
+### What is it?
+### Why did I make this?
+### How did I go about building this?
+### Screenshot of working application and URL link
+### Credits and Licenses
+
+## What is it?
+A simple application using a express.js server to keep your notes organized!
+
+## Why did I make this?
+I wanted a simple application that would be able to keep consistent notes for taks and chores i need to keep on top of. I was tired of pulling out an index card, finding a pencil or pen, and quickly atempting to chicken-scratch some notes about whatever subject i was concerned with, now i can just open up the webpage and write down my thoughts! ( i type much faster than handwriting so this seemed like a win-win for me)
+
+## How did I go about building this?
+This application was built with express.js. Javascript and some html. The application will also be deployed on Heroku!
+
+## Screenshot of working application and URL link
+![ScreenShot of Deployed Application](/)
+
+Deployed Website Link [Click Here!](https://mrsinzo.github.io/Sinzo-Password-Generator/)
+
+## Credits and Licenses
+Using MIT License [Check it here!](https://opensource.org/licenses/MIT)
+
+Creator- Kyle Feldman - [LinkedIn Profile Link](https://www.linkedin.com/in/kyle-feldman-427b5624b)
--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const fs = require("fs");
-// const { Server } = require("http");
 const path = require("path");
 const uuid = require("./helpers/uuid");
-const app = express();
+let api = require('./db/db.json') // was using "const" but i think i need "let" to allow the data to get updated with new notes?
 const PORT = 3002;
-const api = require('./db/db.json')
-// const router = require("./notes")
-// const router = require("express").Router();
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // line 53 of readme/criteria
 app.get("/notes", (req, res) => {
+  console.info(`${req.method} Succesfully opened /notes page!`)
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
@@ -21,31 +19,12 @@ app.get("/notes", (req, res) => {
 /*api routes*/
 
 // line 59 of readme/criteria
-
-
-// app.get("/api/notes", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../public/notes.html"));
-// });
-
-// const result = findById(req.params.id, notes);
-// if (result) {
-//   res.json(result);
-// } else {
-//   res.sendStatus(404);
-// }
-
-// app.get("/api/notes", (req, res) => {
-//   console.log("line 38 returned")
-//   let results = res.json(path.join(__dirname, "./db/db.json"));
-//   res.json(results);
-// });
-
 app.get("/api/notes", (req, res) => {
-  console.info(`${req.method} request received to get reviews`);
-  return res.status(200).json(api)
+  console.info(`${req.method} line23 request received to get Notes`)
+  return res.status(200).json(api);
 });
 
-/* line 61? of readme/criteria // successfully updating db.json now */
+/* line 61? of readme/criteria // successfully updating db.json now// but not updating webpage? */
 app.post("/api/notes", (req, res) => {
   console.info(`${req.method} request received to add a Note/Task`);
 
@@ -62,12 +41,14 @@ app.post("/api/notes", (req, res) => {
     };
 
     fs.readFile("./db/db.json", "utf8", (err, data) => {
+      console.log("line 44")
+      console.log(data)
       if (err) {
         console.error(err);
       } else {
         // Convert string into JSON object
-        const parsedDb = JSON.parse(data);
-        parsedDb.push(newNote);
+        const parsedDb = JSON.parse(data); /** we tell the app we want to put the parsed json file into a variable */
+        parsedDb.push(newNote); /**we push the new note to the file */
         fs.writeFile(
           "./db/db.json",
           JSON.stringify(parsedDb, null, 4),
@@ -76,20 +57,23 @@ app.post("/api/notes", (req, res) => {
               ? console.error(writeErr)
               : console.info("Successfully updated Notes!")
         );
-      }
+        console.log("line 60")
+        console.log(parsedDb)
+      } 
+      
     });
     const response = {
       status: "success",
       body: newNote,
     };
-
+    res.sendFile(path.join(__dirname, "public/notes.html")); // should be aquring new db file to display on page?
     console.log(response);
     res.status(201).json(response);
   } else {
-    res.status(500).json("Error in posting review");
+    res.status(500).json("Error in posting Note");
   }
 });
-
+/**Sets the server to listen at port = 3002 */
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
