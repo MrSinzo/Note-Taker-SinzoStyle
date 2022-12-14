2969ea5b4268d85062af524af4f43ad6b89812d1
Kyle Feldman <flip93wolf@gmail.com>
2022-12-13 18:19:01 -0800

    Finally got the help i needed to figure out the buggit add -A!

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require("path");
 const uuid = require("./helpers/uuid");
 const apiDb = require("./db/db.json");
 const { parse } = require("path"); // no idea where this came from, literally appeared in the file out of thin air
+const { notStrictEqual } = require("assert"); // no idea where this came from, literally appeared in the file out of thin air
+const { json } = require("express");
 const PORT = process.env.PORT || 3002;
 const app = express();
 app.use(express.json());
@@ -12,87 +14,41 @@ app.use(express.static("public"));
 
 // line 53 of readme/criteria
 app.get("/notes", (req, res) => {
-  console.info(`${req.method} line 14 Succesfully opened /notes page!`);
+  console.info(`${req.method} line 16 Succesfully opened /notes page!`);
   res.sendFile(path.join(__dirname, "public/notes.html"));
+  // console.log("line 18");
+  // console.log(__dirname); //C:\Users\flip9\bootcamp\Note-Taker-SinzoStyle
+  // console.log(public/notes.html) says public is not defined?
 });
 
 /*api routes*/
-
-// line 59 of readme/criteria
 /**GET ***** original set up?*/
-app.get("/api/notes", (req, res) => {
-  console.info(`${req.method} line 23 request received to get Notes`);
-  return res.json(apiDb);
-});
-
 // line 59 of readme/criteria
 app.get("/api/notes", (req, res) => {
-  console.info(`${req.method} line 29 request received to add a Note/Task`);
-
-  // Destructuring assignment for the items in req.body
-  const { title, text } = req.body;
-  console.log("line 32");
-  console.log(title);
-  console.log(text);
-  // If all the required properties are present
-  if (title && text) {
-    // Variable for the object we will save
-    const newNote = {
-      title,
-      text,
-      review_id: uuid(), //pulled in helper function from act. 20
-    };
-
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      console.log("line 44");
-      console.log(data);
-      if (err) {
-        console.error(err);
-      } else {
-        // Convert string into JSON object
-        const oldDb =
-          JSON.parse(
-            data
-          ); /** we tell the app we want to put the parsed json file into a variable */
-        oldDb.push(newNote); /**we push the new note to the file */
-        fs.writeFile(
-          "./db/db.json",
-          JSON.stringify(oldDb, null, 4),
-          (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info("Successfully updated Notes!")
-        );
-        console.log("line 63");
-        console.log(oldDb);
-      }
-    });
-    const response = {
-      status: "success",
-      body: newNote,
-    };
-    console.log(response);
-    res.status(201).json(response);
-    // return res.status(200).json(data);
-  } else {
-    res.status(500).json("Error in posting Note");
-  }
+  console.info(`${req.method} //  line 28 request received to get Notes`);
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      data = JSON.parse(data)
+      return res.json(data);
+    }
+  })
 });
+
 
 /* POST*** line 61? of readme/criteria // successfully updating db.json now// but not updating webpage? */
 app.post("/api/notes", (req, res) => {
-  console.info(`${req.method} line 74 request received to add a Note/Task`);
-
+  console.info(`${req.method} //  line 35 request received to add a Note/Task`);
   // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
-
   // If all the required properties are present
   if (title && text) {
     // Variable for the object we will save
     const newNote = {
       title,
       text,
-      review_id: uuid(), //pulled in helper function from act. 20
+      id: uuid(), //pulled in helper function from act. 20
     };
 
     fs.readFile("./db/db.json", "utf8", (err, data) => {
@@ -100,16 +56,13 @@ app.post("/api/notes", (req, res) => {
         console.error(err);
       } else {
         // Convert string into JSON object
+        /** we tell the app we want to put the parsed json file into a variable */
         const parsedDb =
           JSON.parse(
             data
-          ); /** we tell the app we want to put the parsed json file into a variable */
-        console.log("line 107");
-        console.log(parsedDb); //checking unmodified data
-        console.log(newNote); //checking what was inputted
-        parsedDb.push(newNote); /**we push the new note to the file */
-        console.log("line 111");
-        console.log(parsedDb); // checking to see if data was modified
+          ); 
+        /**we push the new note to the parsedDb variable */
+        parsedDb.push(newNote); 
         fs.writeFile(
           "./db/db.json",
           JSON.stringify(parsedDb, null, 4),
@@ -118,21 +71,24 @@ app.post("/api/notes", (req, res) => {
               ? console.error(writeErr)
               : console.info("Successfully updated Notes!")
         );
-        console.log("line 115");
+        console.log(" POST line 79****************");
         console.log(parsedDb);
       }
     });
-    const response = {
+    // no idea what this const response is doing here
+    const response = { 
       status: "success",
       body: newNote,
     };
-    console.log("line 123");
+    console.log("line 82");
     console.log(response);
+    // no idea what these few linesare doing either.
     res.status(201).json(response);
   } else {
     res.status(500).json("Error in posting Note");
   }
 });
+
 /**Sets the server to listen at port = 3002 */
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
